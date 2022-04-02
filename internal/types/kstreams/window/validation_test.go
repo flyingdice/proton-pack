b37d03a2a1128fdc68f19228d0f832265fc3e5a7
Andrew Hawker <andrew.r.hawker@gmail.com>
2022-04-02 09:56:59 -0700

    feat: add time window (#16)

---
+++ internal/types/kstreams/window/validation_test.go
@@ -0,0 +1,35 @@
+package window
+
+import (
+	"errors"
+	"testing"
+	"testing/quick"
+	"time"
+)
+
+// TestValidation_NewWindow checks that default validation checks are run.
+func TestValidation_NewWindow(t *testing.T) {
+	checker := func(lo, hi int64) bool {
+		_, err := NewWindow(time.UnixMicro(lo), time.UnixMicro(hi))
+		if lo < 0 {
+			if !errors.Is(err, ErrLoMustBeNewerThanUnixEpoch) {
+				t.Errorf("expected %v when lo < 0, got %v", ErrLoMustBeNewerThanUnixEpoch, err)
+				return false
+			}
+		} else if hi < lo {
+			if !errors.Is(err, ErrHiGreaterThanLo) {
+				t.Errorf("expected %v when lo > hi, got %v", ErrHiGreaterThanLo, err)
+				return false
+			}
+		} else {
+			if err != nil {
+				t.Errorf("expected no errors, got %v", err)
+				return false
+			}
+		}
+		return true
+	}
+	if err := quick.Check(checker, nil); err != nil {
+		t.Error(err)
+	}
+}
